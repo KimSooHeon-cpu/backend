5e426dea34c12e110dfb1f085f676dca4ccce529
javateacher <javateacher@naver.com>
2025-11-07 14:12:22 +0900

    new_patch

--- src/main/java/com/gym/controller/HomeController.java
+++ src/main/java/com/gym/controller/HomeController.java
@@ -14,7 +14,8 @@ public class HomeController {
 	public String home() {
 		
 		// return "젠킨스와 정상 연동되는지 확인";
-		return "index"; // 메인 홈페이지 이동
+		// return "index"; // 메인 홈페이지 이동
+		return "demo"; // 메인 홈페이지 이동
 	}
 	
 	// 푸시가 안되고 있음, non-fast-forward 상태...이유는? 
