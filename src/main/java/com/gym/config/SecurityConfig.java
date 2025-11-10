419b790a4479738863f4f1b02302fb7c2f1e9ffd
javateacher <javateacher@naver.com>
2025-11-10 14:47:56 +0900

    upload

--- src/main/java/com/gym/config/SecurityConfig.java
+++ src/main/java/com/gym/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
         return (web) -> web.ignoring().requestMatchers("/",
         		"/login",
         		"/board/**",
+        		"/cms/**",
         		"/images/**",               
                 "/static/**", 
                 "/css/**", 
