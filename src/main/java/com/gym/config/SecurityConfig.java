07ec5b07ada4bb062c41d8ddb37af3206a329e9a
javateacher <javateacher@naver.com>
2025-11-10 14:35:25 +0900

    upload

--- src/main/java/com/gym/config/SecurityConfig.java
+++ src/main/java/com/gym/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/",
+        		"/login",
         		"/images/**",               
                 "/static/**", 
                 "/css/**", 
