b5da3debee498afc77be8dce62765fc2cff5e70c
javateacher <javateacher@naver.com>
2025-11-10 17:00:25 +0900

    upload

--- src/main/java/com/gym/config/WebConfig.java
+++ src/main/java/com/gym/config/WebConfig.java
@@ -49,8 +49,20 @@ public class WebConfig implements WebMvcConfigurer {
 		
 		// WebMvcConfigurer.super.addViewControllers(registry);
 		
-		//registry.addViewController("/{path:[^\\.]*}").setViewName("forward:/index.html");
-		//registry.addViewController("/**/{path:[^\\.]*}").setViewName("forward:/index.html");
+		registry.addViewController("/")
+        		.setViewName("forward:/index.html");
+
+		// Map "/word", "/word/word", and "/word/word/word" - except for anything starting with "/api/..." or ending with
+		// a file extension like ".js" - to index.html. By doing this, the client receives and routes the url. It also
+		// allows client-side URLs to be bookmarked.
+		
+		// Single directory level - no need to exclude "api"
+		registry.addViewController("/{x:[\\w\\-]+}")
+		        .setViewName("forward:/index.html");
+		
+		// Multi-level directory path, need to exclude "api" on the first part of the path
+		registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}")
+		        .setViewName("forward:/index.html");
 	}
 	
 	
