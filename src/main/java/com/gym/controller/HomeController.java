0392ed74dafded61e0ec6bc50a0473374e127134
javateacher <javateacher@naver.com>
2025-11-10 15:59:55 +0900

    new_upload

--- src/main/java/com/gym/controller/HomeController.java
+++ src/main/java/com/gym/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.gym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 // [251105] 일반 테스트용 홈 컨트롤러
 // 목적 : 젠킨스와 정상 연동되는지 확인하는 용도
@@ -10,12 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HomeController {
 	
-	@GetMapping({"/", "/board"})
+	@GetMapping({"/", "/{path:^(?!api).*}"})
 	public String home() {
 		
-		// return "젠킨스와 정상 연동되는지 확인";
 		return "index"; // 메인 홈페이지 이동
-		// return "demo"; // 메인 홈페이지 이동
 	}
 	
 	// 푸시가 안되고 있음, non-fast-forward 상태...이유는? 
