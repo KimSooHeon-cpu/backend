f05d2cd3842ee62589e27b0adc0a872fe84493b8
javateacher <javateacher@naver.com>
2025-11-11 12:12:42 +0900

    /cms 관련 주소 관리자만 접근 가능

--- src/main/java/com/gym/controller/HomeController.java
+++ src/main/java/com/gym/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.gym.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +15,23 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	
 	// @GetMapping({"/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
-	@GetMapping({"/", "/board/**", "/facilities/**", "/contents/**", "/login", "/join", "/mypage", "/cms/**" })
+	@GetMapping({"/", "/board/**", "/facilities/**", "/contents/**", "/login", "/join", "/mypage", "/cms/login" })
 	public String home() {
 		
 		return "index"; // 메인 홈페이지 이동
 	}
 	
-//	@GetMapping({"/cms", "/cms/home", "/cms/user", "/cms/facility"})
-//	public String cms(Authentication auth) {
-//		
-//		log.info("auth : " + auth);
-//		
-//		String movePath = "/cms/login";
-//		// 관리자로 로그인 되었으면 본화면으로 진입 그렇지 않으면 로그인 화면으로 진입
-//		
-//		
-//		return "redirect:/" + movePath; // 관리자 로그인 페이지로 이동
-//	}
+	@PreAuthorize("hasRole('ROLE_ADMIN')") // 관리자 권한만 접근 가능
+	@GetMapping({"/cms", "/cms/home", "/cms/user", "/cms/facility", "/cms/reservation"})
+	public String cms(Authentication auth) {
+		
+		log.info("auth : " + auth);
+		
+		return "index";
+		//String movePath = "/cms/login";
+		// 관리자로 로그인 되었으면 본화면으로 진입 그렇지 않으면 로그인 화면으로 진입
+		//return "redirect:/" + movePath; // 관리자 로그인 페이지로 이동
+	}
 	
 	// 푸시가 안되고 있음, non-fast-forward 상태...이유는? 
 	
