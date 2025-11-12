b31d57a43ff8a11c1850bf357e949a40024b5cce
javateacher <javateacher@naver.com>
2025-11-12 15:45:11 +0900

    CORS대비

--- src/main/java/com/gym/config/SecurityConfig.java
+++ src/main/java/com/gym/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://16.176.33.172");
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedOriginPattern("*"); // 모든 Origin 허용
         configuration.addAllowedMethod("*");        // 모든 HTTP Method 허용
         configuration.addAllowedHeader("*");        // 모든 Header 허용
